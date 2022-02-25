4bb9050fc8602f994eeda41a36c6084419b34fb6
Rahul-24Kumar <24rahul.pcm@gmail.com>
2022-02-25 13:30:54 +0530

    Assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,73 +1,119 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
-})
+let players =
+   [
+       {
+           "name": "rahul",
+           "dob": "18/09/1985",
+           "gender": "male",
+           "city": "bihar",
+           "sports": ["swimming"],
+           "bookings": [
+               {
+                   "bookingNumber": 1,
+                   "sportId": "",
+                   "centerId": "",
+                   "type": "private",
+                   "slot": '16286598000000',
+                   "bookedOn": '31/08/2021',
+                   "bookedFor": '01/09/2021'
+               },
+               {
+                   "bookingNumber": 2,
+                   "sportId": "",
+                   "centerId": "",
+                   "type": "private",
+                   "slot": '16286518000000',
+                   "bookedOn": '31/08/2001',
+                   "bookedFor": '01/09/2001'
+               },
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": ["soccer"],
+           "bookings": []
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": ["tennis"],
+           "bookings": []
+       },
+       {
+        "name": "abhhishek",
+        "dob": "1/12/1990",
+        "gender": "male",
+        "city": "mumbai",
+        "sports": ["cricket"],
+        "bookings": []
+    },
+    {
+        "name": "shrikant",
+        "dob": "18/08/1994",
+        "gender": "male",
+        "city": "aurangabad",
+        "sports": ["football"],
+        "bookings": [
+            {
+                "bookingNumber": 7,
+                "sportId": "",
+                "centerId": "",
+                "type": "private",
+                "slot": '16286598000000',
+                "bookedOn": '31/08/2021',
+                "bookedFor": '31/10/2021'
+            },
+        ]
+    },
+   ]
 
-router.get("/random" , function(req, res) {
-    res.send("hi there")
-})
+   router.post("/players", function(req, res) {
+       let value =req.body.name
+       let flag= true
+       for (let i=0; i<players.length; i++) {
+           if (players[i].name===value) {
+               flag=false
+               res.send("Error player name already exist");
+           }
+       }
+       if (flag === true) {
+           players.push(req.body);
+           res.send(players);
 
+       }
+   })
 
-router.get("/test-api" , function(req, res) {
-    res.send("hi FunctionUp")
-})
+   router.post("/:playersName/bookings/:bookingId", function(req, res) {
+    let name =req.params.playersName
+    let id =req.params.bookingId
+    let value=req.body
+    for (let i=0; i<players.length; i++) {
+        if (players[i].name===name) {
+            let arr=players[i].bookings
+            if(arr.length===0) {
+                arr.push(value)
+                res.send(players)
+            } else {
+                for (let j=0; j<arr.length; j++) {
+                    if(arr[j].bookingNumber != id) {
+                        arr.push(value)
+                        res.send(players)
+                    } else {
+                        res.send("booking ID already exists")
+                    }
+                }
+            }
+        }
+    }
+    res.send("name does not exist")
+   
+   })
 
-
-router.get("/test-api-2" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API")
-})
-
-
-router.get("/test-api-3" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
-
-
-router.get("/test-api-4" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-
-
-router.get("/test-api-5" , function(req, res) {
-    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
-})
-
-router.post("/test-post", function(req, res) {
-    res.send([ 23, 45 , 6])
-})
-
-
-router.post("/test-post-2", function(req, res) {
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-router.post("/test-post-3", function(req, res) {
-    // let id = req.body.user
-    // let pwd= req.body.password
-
-    // console.log( id , pwd)
-
-    console.log( req.body )
-
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-
-
-router.post("/test-post-4", function(req, res) {
-    let arr= [ 12, "functionup"]
-    let ele= req.body.element
-    arr.push(ele)
-    res.send(  { msg: arr , status: true }  )
-})
-
-module.exports = router;
+   module.exports = router;
