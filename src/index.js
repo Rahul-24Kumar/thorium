50ea081665b09c34de551e1622dda2e724529521
Rahul-24Kumar <24rahul.pcm@gmail.com>
2022-02-27 11:13:52 +0530

    workDone

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/rahul123456-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
+
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
@@ -20,3 +21,5 @@ app.use('/', route);
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
+
+// mongoose schema
